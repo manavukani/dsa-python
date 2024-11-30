1086ef32063ac3bd04391c10d364ac0bb104477e
Manav Nitesh Ukani <84531789+manavukani@users.noreply.github.com>
2024-11-29 19:28:22 -0500

    LL medium last 2 done

---
+++ A2Z/4 Linked List/2 Medium LL/14 add_1_to_a_number.py
@@ -0,0 +1,47 @@
+class Node:
+    def __init__(self, val, next):
+        self.next = next
+        self.val = val
+
+# TC = O(3N) ---> reverse + traverse + reverse
+# SC = O(1)
+def brute(head):
+    def reverse_LL(head):
+        curr = head
+        prevNode = None
+        while curr:
+            nextNode = curr.next
+            curr.next = prevNode
+            prevNode = curr
+            curr = nextNode
+        return prevNode
+    head = reverse_LL(head)
+
+    curr = head
+    carry = 1  # we add 1
+
+    while curr:
+        curr.val = curr.val + carry
+        if curr.val < 10:
+            carry = 0
+            break
+        else:
+            curr.val = 0
+            carry = 1
+        curr = curr.val
+
+    # 2 cases: we break or completion done
+    # if carry exists at end
+    if carry == 1:
+        newNode = Node(1)
+        head = reverse_LL(head)
+        newNode.next = head
+        return newNode
+    # if not carry
+    else:
+        head = reverse_LL(head)
+        return head
+
+# without reversing --- need to use recursion (backtracking)
+def optimal(head):
+    pass
