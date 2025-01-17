68bc273eeb024aebbb1229d0e1213819a6ef2109
Manav Ukani <84531789+manavukani@users.noreply.github.com>
2025-01-17 11:38:07 -0500

    added test cases

--- A2Z/13 Graphs/3 Shortest Path/12 WEIRD_minimum_multiplications_start_end.py
+++ A2Z/13 Graphs/3 Shortest Path/12 WEIRD_minimum_multiplications_start_end.py
@@ -1,16 +1,17 @@
 # TODO: at each step, start is multiplied with any number in the array and then mod operation with 100000 is done to get the new start.
 # output: find the minimum steps in which end can be achieved starting from start. if not possible, return -1.
 
-'''
+"""
 INTUTION:
 - similar to shortest path, check for all nei (all the elements in arr)
 - if we encounter same val with more steps we ignore repeating calculation, not push to queue
 - if found same val with less steps, we add to dict and queue
 - dict is better than array of length 10**5, cause array will be sparse
 - since we only move 1 step each time, no need to PQ
-'''
+"""
 
 from collections import deque
+
 
 class Solution:
     def minimumMultiplications(self, arr, start, end):
@@ -21,7 +22,7 @@ class Solution:
         MOD = 100000
         q = deque()
         q.append((start, 0))  # (current value, steps taken)
-        
+
         dist = {}
         dist[start] = 0
 
@@ -31,15 +32,22 @@ class Solution:
             # checking all nei
             for ele in arr:
                 num = (ele * val) % MOD  # next number
-                
+
                 if num not in dist or steps + 1 < dist[num]:
                     dist[num] = steps + 1
-                    
+
                     # If we reach the end, return the steps
                     if num == end:
                         return steps + 1
-                    
+
                     q.append((num, steps + 1))
 
         # unreachable, return -1
         return -1
+
+
+# test
+arr = [3, 4, 65]
+start = 7
+end = 66175
+print(Solution().minimumMultiplications(arr, start, end))  # 2
--- A2Z/13 Graphs/3 Shortest Path/13 network_delay_time.py
+++ A2Z/13 Graphs/3 Shortest Path/13 network_delay_time.py
@@ -1,15 +1,16 @@
 # input: times = [(ui, vi, wi),....] -- u --> v with wt = wi, n = nodes (1 to n), k = src
 # output: minimum time needed for signal to reach all node from k, if not possible return -1
 
-'''
+"""
 INTUTION:
 - we need to minimize the max time taken
 - use PQ and find shortest distance (time taken) to each node from src using dijkstra
 - PQ/min-heap based on time, pick smaller time first
 - check for each node in PQ, if any nei has better time than current cur stored in array
-'''
+"""
 
 import heapq
+
 
 # TC = O(E log V)    ---->    (same as normal dijkstra)
 def networkDelayTime(times, n, k):
@@ -20,13 +21,13 @@ def networkDelayTime(times, n, k):
     :rtype: int
     """
     # convert edges to adj list
-    adj = [[] for _ in range(n+1)] # 1-based indexing
-    for u,v,wt in times:
+    adj = [[] for _ in range(n + 1)]  # 1-based indexing
+    for u, v, wt in times:
         adj[u].append((v, wt))
-    
+
     min_heap = [(0, k)]  # (current time, node)
 
-    dist = [float('inf')]*(n+1)
+    dist = [float("inf")] * (n + 1)
     dist[k] = 0
 
     while min_heap:
@@ -42,7 +43,14 @@ def networkDelayTime(times, n, k):
             if new_time < dist[nei]:
                 dist[nei] = new_time
                 heapq.heappush(min_heap, (new_time, nei))
-    
+
     # time needed for all nodes to recieve signal = max time for any node
     max_time = max(dist[1:])  # ignore the 0th index
-    return -1 if max_time == float('inf') else max_time
+    return -1 if max_time == float("inf") else max_time
+
+
+# test
+times = [[2, 1, 1], [2, 3, 1], [3, 4, 1]]
+n = 4
+k = 2
+print(networkDelayTime(times, n, k))  # 2
