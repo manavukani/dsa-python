bd5d88009bad9812c3d0d6a3680569d2ffd814ae
Manav Ukani <84531789+manavukani@users.noreply.github.com>
2025-01-31 14:21:00 -0800

    recursion started

---
+++ A2Z/14 DP/1 basics-fibonnaci.py
@@ -0,0 +1,52 @@
+# memoization -> tabulation -> space optimization
+
+def recursiveFib(n):
+    if n <= 1:
+        return 1
+    return recursiveFib(n-1) + recursiveFib(n-2)
+
+def memoizationFib(n, dp):
+    if n <= 1:
+        return n
+    
+    if dp[n] != -1:
+        return dp[n]
+    dp[n] = memoizationFib(n-1, dp) + memoizationFib(n-2, dp)
+    return dp[n]
+
+# print("Memoization: TC = N, SC = N")
+# n = 5
+# dp = [-1] * (n+1)
+# print(memoizationFib(n, dp))
+
+def tabulationFib(n):
+
+    if n<=1:
+        return n
+    
+    dp = [-1]*(n+1)
+
+    dp[0] = 0
+    dp[1] = 1
+
+    for i in range(2, n+1):
+        dp[i] = dp[i-1] + dp[i-2]
+    
+    return dp[n]
+
+print("Tabulation: TC = N, SC = N")
+print(tabulationFib(8))
+
+def spaceOptimizedFib(n):
+    prev2 = 0
+    prev = 1
+
+    for _ in range(2, n+1):
+        cur_i = prev2 + prev
+        prev2 = prev
+        prev = cur_i
+    
+    return prev
+
+print("Space Optimized: TC = N, SC = 1")
+print(spaceOptimizedFib(8))
---
+++ A2Z/14 DP/2 climbing_stairs.py
@@ -0,0 +1,26 @@
+class RecursiveSolution:
+    def climbStairs(self, n: int) -> int:
+        if n == 0:
+            return 1
+        if n == 1:
+            return 1
+        
+        # left and right sub tree for recursion
+        left = self.climbStairs(n-1)
+        right = self.climbStairs(n-2)
+
+        return left + right
+
+# Notice - It's similar to Fibonnaci recursion
+
+class SpaceOptimizedSolution:
+    def climbStairs(self, n: int) -> int:
+        one = 1
+        two = 1
+
+        for i in range(n-1):
+            tmp = one
+            one = one + two
+            two = tmp
+        
+        return one
