1b48c4bf4053ac66c49e78f0701e1c0a2392b5e0
Manav Ukani <84531789+manavukani@users.noreply.github.com>
2025-01-31 17:35:31 -0500

    dp started

--- A2Z/14 DP/2 climbing_stairs.py
+++ A2Z/14 DP/2 climbing_stairs.py
@@ -4,23 +4,25 @@ class RecursiveSolution:
             return 1
         if n == 1:
             return 1
-        
+
         # left and right sub tree for recursion
-        left = self.climbStairs(n-1)
-        right = self.climbStairs(n-2)
+        left = self.climbStairs(n - 1)
+        right = self.climbStairs(n - 2)
 
         return left + right
 
+
 # Notice - It's similar to Fibonnaci recursion
+
 
 class SpaceOptimizedSolution:
     def climbStairs(self, n: int) -> int:
         one = 1
         two = 1
 
-        for i in range(n-1):
+        for i in range(n - 1):
             tmp = one
             one = one + two
             two = tmp
-        
+
         return one
